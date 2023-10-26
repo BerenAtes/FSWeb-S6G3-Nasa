404d32446c782cd1ed43f881cbfda06170f0b76c
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2023-10-27 00:44:55 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,1 @@
-import React from "react";
 
-function Footer(props) {
-  return (
-    <div>
-      <div>{props.data.copyright}</div>
-      <div>{props.data.service_version}</div>
-    </div>
-  );
-}
-export default Footer;
